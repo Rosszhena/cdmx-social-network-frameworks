ad76f13763208d7f7bbb8122e36154bd05be5da8
Maria Rosa Rosete <ross.zhena@gmail.com>
2018-09-21 00:17:47 -0500

    Anadimos navbar y rutas

--- RedSocial/src/app/app-routing.module.ts
+++ RedSocial/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomePageComponent} from './componentes/home-page/home-page.component';
+import {LoginPageComponent} from './componentes/login-page/login-page.component';
+import {RegisterPageComponent} from './componentes/register-page/register-page.component';
+import {PrivadoPageComponent} from './componentes/privado-page/privado-page.component';
+import { NotFoundPafeComponent } from './componentes/not-found-pafe/not-found-pafe.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomePageComponent},
+  {path: 'login', component: LoginPageComponent},
+  {path: 'register', component: RegisterPageComponent},
+  {path: 'privado', component: PrivadoPageComponent},
+  {path: '**', component: NotFoundPafeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
